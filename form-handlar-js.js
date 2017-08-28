4b6e46a2de04f768219b3888ec8ea57f05682b6a
Sukanta Bala <sukantabalacste2016@gmail.com>
2017-08-28 11:16:18 +0600

    upadeted form handler

--- form-handlar-js.js
+++ form-handlar-js.js
@@ -70,8 +70,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
     xhr.onreadystatechange = function() {
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
-        document.getElementById('gform').style.display = 'none'; // hide form
-        document.getElementById('thankyou_message').style.display = 'block';
+        document.getElementById('thankyou_message_assessment').style.display = 'block';
         return;
     };
     // url encode form data for sending as post data
