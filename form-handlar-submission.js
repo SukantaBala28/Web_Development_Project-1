5f16f25ad3a12fc429de55077f9f4bd04a67f5a8
Sukanta Bala <sukantabalacste2016@gmail.com>
2017-10-26 16:43:21 +0600

    Update form-handlar-submission.js

--- form-handlar-submission.js
+++ form-handlar-submission.js
@@ -70,6 +70,7 @@ function handleFormSubmit(event) {  // handles form submit withtout any jquery
         console.log( xhr.status, xhr.statusText )
         console.log(xhr.responseText);
         document.getElementById('thankyou_message').style.display = 'block';
+        document.getElementById('thankyou_message_assessment').style.display = 'block';
         return;
     };
     // url encode form data for sending as post data
